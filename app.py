204d767152b6337c5cf6dfdbd6eab181f75dd952
sohamm <sohamgurav456123@gmail.com>
2026-02-24 09:00:01 +0530

    Changes

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/') 
 def index():
-    return "Soham"
+    return "Soham  2410825"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
